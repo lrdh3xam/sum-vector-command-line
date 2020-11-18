4822e0711d9fd311ae07937e34f877dfda8ef3f6
Brandon <fsc7p2ioxntf@protonmail.com>
2020-11-18 15:38:06 -0500

    Completed challenge.
    Hacker rank sum array elements.

---
+++ sum-vector.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import os
+import sys
+
+def simpleArraySum(ar):
+    return sum(ar)
+
+if __name__ == '__main__':
+
+    ar_count = int(input())
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+    print(str(result))
