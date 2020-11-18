7309a4992f0f7f7bedd11887c8a71214302d82db
Brandon <fsc7p2ioxntf@protonmail.com>
2020-11-18 15:42:24 -0500

    Update sum-vector.py
    Added commenting.

--- sum-vector.py
+++ sum-vector.py
@@ -9,6 +9,11 @@ def simpleArraySum(ar):
 if __name__ == '__main__':
 
     ar_count = int(input())
+
+    #
+    # Add integers to a list stripping EOL chars and split based
+    # on spaces.
+    #  
     ar = list(map(int, input().rstrip().split()))
 
     result = simpleArraySum(ar)
